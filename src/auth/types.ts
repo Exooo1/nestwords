7af483ab1862bcda87fa9de5b62f96339d0d9a19
Maskalenchik Vlas <vlasmaskalenchik1998@gmail.com>
2023-06-04 12:26:15 +0300

    removed unnecessary type

--- src/auth/types.ts
+++ src/auth/types.ts
@@ -1,47 +1,13 @@
 import { EmailDTO, SignUpDTO } from './auth.dto';
 import { TStatusRes } from '../utils/status';
-import { TAccountWord } from '../schemas/auth/types';
+import { CAccountProfile } from "../schemas/auth/types";
 
 export interface IAuthController {
   signUp: (data: SignUpDTO) => Promise<TStatusRes<string>>;
 }
 
-type TProfile = {
-  firstName: string;
-  lastName: string;
-  totalWords: number;
-  words: {
-    a: Array<TAccountWord>;
-    b: Array<TAccountWord>;
-    c: Array<TAccountWord>;
-    d: Array<TAccountWord>;
-    e: Array<TAccountWord>;
-    f: Array<TAccountWord>;
-    g: Array<TAccountWord>;
-    h: Array<TAccountWord>;
-    i: Array<TAccountWord>;
-    j: Array<TAccountWord>;
-    k: Array<TAccountWord>;
-    l: Array<TAccountWord>;
-    m: Array<TAccountWord>;
-    n: Array<TAccountWord>;
-    o: Array<TAccountWord>;
-    p: Array<TAccountWord>;
-    q: Array<TAccountWord>;
-    r: Array<TAccountWord>;
-    s: Array<TAccountWord>;
-    t: Array<TAccountWord>;
-    u: Array<TAccountWord>;
-    v: Array<TAccountWord>;
-    w: Array<TAccountWord>;
-    x: Array<TAccountWord>;
-    y: Array<TAccountWord>;
-    z: Array<TAccountWord>;
-  };
-};
-
 export interface IAuthService {
-  profile: TProfile;
+  profile: CAccountProfile;
   signUp: (data: SignUpDTO) => Promise<TStatusRes<string>>;
   sendEmail: (data: EmailDTO) => Promise<TStatusRes<null>>;
   login: () => Promise<TStatusRes<string>>;
